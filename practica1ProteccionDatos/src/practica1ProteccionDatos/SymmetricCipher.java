81c468b901b5ce97f51a18781d2d3698dfb2af38
Ignacio Lázaro <ilazaro@altano.es>
2018-09-23 17:35:48 +0200

    Proyecto

---
+++ practica1ProteccionDatos/src/practica1ProteccionDatos/SymmetricCipher.java
@@ -0,0 +1,70 @@
+package practica1ProteccionDatos;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.*;
+import java.security.InvalidKeyException;
+
+public class SymmetricCipher {
+
+	byte[] byteKey;
+	SymmetricEncryption s;
+	SymmetricEncryption d;
+	
+	// Initialization Vector (fixed)
+	
+	byte[] iv = new byte[] { (byte)49, (byte)50, (byte)51, (byte)52, (byte)53, (byte)54, 
+		(byte)55, (byte)56, (byte)57, (byte)48, (byte)49, (byte)50, (byte)51, (byte)52,
+		(byte)53, (byte)54};
+
+    /*************************************************************************************/
+	/* Constructor method */
+    /*************************************************************************************/
+	public void SymmetricCipher() {
+	}
+
+    /*************************************************************************************/
+	/* Method to encrypt using AES/CBC/PKCS5 */
+    /*************************************************************************************/
+	public byte[] encryptCBC (byte[] input, byte[] byteKey) throws Exception {
+		
+		byte[] ciphertext = null;	
+		
+		
+			// Generate the plaintext with padding
+			
+			
+	
+			// Generate the ciphertext
+			
+		
+		
+		return ciphertext;
+	}
+	
+	/*************************************************************************************/
+	/* Method to decrypt using AES/CBC/PKCS5 */
+    /*************************************************************************************/
+	
+	
+	public byte[] decryptCBC (byte[] input, byte[] byteKey) throws Exception {
+	
+		
+		byte [] finalplaintext = null;
+		
+			
+		// Generate the plaintext
+			
+		
+		// Eliminate the padding
+		
+			
+		
+		return finalplaintext;
+	}
+	
+}
+
